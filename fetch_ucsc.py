168f5c11388897a251d4764fbc4ab3059bc13b45
Giovanni Dall'Olio <giovanni.dallolio@upf.edu>
2011-11-25 15:45:23 +0100

    ADD: function to initialize Browser object

--- fetch_ucsc.py
+++ fetch_ucsc.py
@@ -17,6 +17,8 @@ Input files:
 
 import optparse
 import sys 
+import mechanize
+import cookielib
 
 
 def get_options():
@@ -45,11 +47,58 @@ def get_options():
 
     return (options, args)
 
+def initialize_browser():
+    """
+    initialize a Browser object.
+
+    Acknowledgements to: http://stockrt.github.com/p/emulating-a-browser-in-python-with-mechanize/
+    """
+
+    br = mechanize.Browser()
+    # Cookie Jar
+    cj = cookielib.LWPCookieJar()
+    br.set_cookiejar(cj)
+
+    # Browser options
+    br.set_handle_equiv(True)
+#    br.set_handle_gzip(True)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
+
+    # Follows refresh 0 but not hangs on refresh > 0
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+    # Want debugging messages?
+#    br.set_debug_http(True)
+#    br.set_debug_redirects(True)
+#    br.set_debug_responses(True)
+
+    # User-Agent (this is cheating, ok?)
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    br.addheaders.append(('email', 'giovanni.dallolio@upf.edu'))
+
+    return br
 
 
-
-
+basicurl = """http://genome.ucsc.edu/cgi-bin/hgTracks?db=hg18&
+    wgRna=hide
+    &cpgIslandExt=pack
+    &ensGene=hide
+    &mrna=hide
+    &intronEst=hide
+    &mgcGenes=hide
+    &hgt.psOutput=on
+    &cons44way=hide
+    &snp130=hide
+    &snpArray=hide
+    &wgEncodeReg=hide
+    &pix=1000
+    &refGene=pack
+    &knownGene=hide
+    &rmsk=hide"""
 
 if __name__ == '__main__':
     get_options()
+    initialize_browser()
 
