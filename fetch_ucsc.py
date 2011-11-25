2e8b1318961419e43a8cd596dd42989f8cf60d30
Giovanni Dall'Olio <giovanni.dallolio@upf.edu>
2011-11-25 13:03:23 +0100

    ADD: stub for fetch_ucsc script

---
+++ fetch_ucsc.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""A script to fetch screenshots from the UCSC browser
+
+Usage:
+======
+    
+    python fetch_ucsc.py --regions <regions file> --options <options file> --ucsc_url <ucsc options file>
+
+Input files:
+============
+
+ * regions file: contains a list of regions to be analyzed
+ * options file: contains which tracks to include, and which options
+ * uscs file: a file containing the url to the UCSC browser, and username/password for custom UCSC instances
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
+
