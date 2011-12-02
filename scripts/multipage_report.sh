36d285ffdf8672842c10bf7ba96930b5ef5a8da5
Giovanni Dall'Olio <giovanni.dallolio@upf.edu>
2011-12-02 11:00:48 +0100

    FIX: documentation and output file name

--- scripts/multipage_report.sh
+++ scripts/multipage_report.sh
@@ -2,7 +2,7 @@
 # Wrapper that uses pdftk and pdfpages.py to convert all the pdf files in a folder to a 2x2 pages report
 #
 # Usage: 
-#   $: bash multipage_report.sh inputfolder outputfilename.pdf
+#   $: bash multipage_report.sh inputfolder outputfilename
 
 
 INPUTFOLDER=$1
@@ -12,4 +12,4 @@ echo pdftk ${INPUTFOLDER}/*.pdf cat output ${OUTPUTFILE}_tmp.pdf
 pdftk ${INPUTFOLDER}/*.pdf cat output ${OUTPUTFILE}_tmp.pdf
 echo python pdfpages.py ${OUTPUTFILE}_tmp.pdf -n2x2
 python pdfpages.py ${OUTPUTFILE}_tmp.pdf -n2x2
-mv ${OUTPUTFILE}-pdf-pages.pdf $2
+mv ${OUTPUTFILE}_tmp-pdf-pages.pdf $2.pdf
